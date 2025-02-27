fa5d3f76437baf40e0a81b6b2154dbb2f89165fa
ZerPm <christian.manco3@gmail.com>
2025-02-27 13:33:56 -0500

    Initial Commit

---
+++ controllers/auth.controller.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+exports.register = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ name, email, password: hashedPassword });
+        await newUser.save();
+        res.status(201).json({ message: 'Usuario registrado' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(400).json({ error: 'Credenciales inválidas' });
+        }
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.json({ token });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
